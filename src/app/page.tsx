2a26207e07638660dc5b3668eec4563f698a4568
Galartza <jotaquin99g@gmail.com>
2025-02-13 15:49:25 -0300

    add card in screen initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 //src\app\page.tsx
 
-import NavigationBar from "@/components/NavigationBar";
-import Link from "next/link";
+import StartCard from "@/components/StartCard/StartCard";
+import 'animate.css';
 
 
 
 export default function Home() {
   return (
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1>bienvenido</h1>
-      <button>
-        <Link href={"/home"}>
-          ¡Vamos!
-        </Link>
-      </button>
+      <main className="flex h-screen justify-center items-center sm:items-center">
+        <StartCard/>
       </main>
   );
 }
