1658c96ea988968e7df87e6b9a06db29152e4cd6
Galartza <jotaquin99g@gmail.com>
2025-02-13 13:07:31 -0300

    add component nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+//src\app\layout.tsx
+
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,9 +9,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className=""
-      >
+      <body className="">
         {children}
       </body>
     </html>
