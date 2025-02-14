f15d5802fa35a6a49808248659ae2cb400f9af1c
Galartza <jotaquin99g@gmail.com>
2025-02-14 16:14:25 -0300

    add more styles in initial screen and navbar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,35 +1,81 @@
-import React from 'react'
-import styles from './NavigationBar.module.css'
-import Link from 'next/link'
-import { FaFolderOpen, FaHome, FaPhone, FaUser } from 'react-icons/fa'
+'use client';
+import React, { useState, useEffect } from 'react';
+import styles from './NavigationBar.module.css';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'; // Importar usePathname
+import 'animate.css';
+import { FaFolderOpen, FaHome, FaPhone, FaUser } from 'react-icons/fa';
+import { useRouter } from 'next/navigation';
 
-const NavigationBar = () => {
+const NavigationBar: React.FC = () => {
+  const [isEntering, setIsEntering] = useState<boolean>(false);
+  const [isExiting, setIsExiting] = useState<boolean>(false);
+  const router = useRouter();
+  const pathname = usePathname(); // Obtener la ruta actual
+
+  useEffect(() => {
+    // Verificamos si hay un indicador en el localStorage
+    const shouldAnimate = localStorage.getItem('shouldAnimateNav');
+    if (shouldAnimate === 'true') {
+      setIsEntering(true); // Activamos la animación de entrada
+      localStorage.removeItem('shouldAnimateNav'); // Eliminamos el indicador
+    }
+  }, []);
+
+  const handleExit = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, path: string) => {
+    if (path === '/') {
+      e.preventDefault(); // Evita la navegación inmediata
+      setIsExiting(true); // Activa la animación de salida
+      setTimeout(() => {
+        router.push('/'); // Redirige después de la animación
+      }, 1000); // Duración que coincide con la animación de salida
+    }
+  };
+
   return (
-    <nav className={styles.nav}>
-        <ul className={styles.navItem}>
-            <li className={styles.navList}>
-                <Link href={'/home'} className={styles.navLink}>
-                    <FaHome  />
-                </Link>
-            </li>
-            <li className={styles.navList}>
-                <Link href={'/about'} className={styles.navLink}>
-                    <FaUser />
-                </Link>
-            </li>
-            <li className={styles.navList}>
-                <Link href={'/portfolio'} className={styles.navLink}>
-                    <FaFolderOpen />
-                </Link>
-            </li>
-            <li className={styles.navList}>
-                <Link href={'/contact'} className={styles.navLink}>
-                    <FaPhone  />
-                </Link>
-            </li>
-        </ul>
+    <nav className={`${styles.nav} animate__animated ${isEntering ? 'animate__zoomInDown' : ''} ${isExiting ? 'animate__zoomOutUp' : ''}`}>
+      <ul className={styles.navItem}>
+        <li className={styles.navList}>
+          <Link
+            href={'/'}
+            className={`${styles.navLink} ${pathname === '/' ? styles.actives : ''}`}
+            onClick={(e) => handleExit(e, '/')}
+            style={{ pointerEvents: isExiting ? 'none' : 'auto' }}
+          >
+            <FaHome className={`${styles.navIcon} ${pathname === '/' ? styles.actives : ''}`} />
+            <p className={`${styles.textNavLink} ${pathname === '/' ? styles.active : ''}`}>home</p>
+          </Link>
+        </li>
+        <li className={styles.navList}>
+          <Link
+            href={'/about'}
+            className={`${styles.navLink} ${pathname === '/about' ? styles.actives : ''}`}
+          >
+            <FaUser className={`${styles.navIcon} ${pathname === '/about' ? styles.actives : ''}`} />
+            <p className={`${styles.textNavLink} ${pathname === '/about' ? styles.active : ''}`}>about me</p>
+          </Link>
+        </li>
+        <li className={styles.navList}>
+          <Link
+            href={'/portfolio'}
+            className={`${styles.navLink} ${pathname === '/portfolio' ? styles.actives : ''}`}
+          >
+            <FaFolderOpen className={`${styles.navIcon} ${pathname === '/portfolio' ? styles.actives : ''}`} />
+            <p className={`${styles.textNavLink} ${pathname === '/portfolio' ? styles.active : ''}`}>portfolio</p>
+          </Link>
+        </li>
+        <li className={styles.navList}>
+          <Link
+            href={'/contact'}
+            className={`${styles.navLink} ${pathname === '/contact' ? styles.actives : ''}`}
+          >
+            <FaPhone className={`${styles.navIcon} ${pathname === '/contact' ? styles.actives : ''}`} />
+            <p className={`${styles.textNavLink} ${pathname === '/contact' ? styles.active : ''}`}>contact</p>
+          </Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default NavigationBar;
--- src/components/StartCard/StartCard.tsx
+++ src/components/StartCard/StartCard.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import styles from './StartCard.module.css';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import 'animate.css';
 import Image from 'next/image';
 
 const StartCard = () => {
+  const [isExiting, setIsExiting] = useState(false);
+  const router = useRouter();
+
+  const handleExit = () => {
+    setIsExiting(true);
+    localStorage.setItem('shouldAnimateNav', 'true'); // Indicador para animar la nav
+    setTimeout(() => {
+      router.push('/about');
+    }, 1000); // Duración que coincide con la animación de salida
+  };
+
   return (
-    <div className={`${styles.card} animate__animated animate__zoomInDown`}>
+    <div className={`${styles.card} animate__animated ${isExiting ? 'animate__zoomOutUp' : 'animate__zoomInDown'}`}>
       <div className={styles.cardBorderTop}></div>
+      <span>¡Bienvenido a mi Portfolio!</span>
       <Image
         className={styles.img}
-        src="/avatar.png" // Ruta correcta de la imagen
-        width={140} // Ajusta el tamaño según sea necesario
-        height={140} // Ajusta el tamaño según sea necesario
-        alt=""
+        src="/avatar.png"
+        width={140}
+        height={140}
+        alt="Avatar"
       />
       <span>Joaquin Galarza</span>
       <p className={styles.job}>Programador Front-End</p>
-      <button className={styles.btnComeOn}>
-        <Link href="/home">¡Vamos!</Link>
+      <button className={styles.btnComeOn} onClick={handleExit}>
+        ¡Vamos!
       </button>
     </div>
   );
